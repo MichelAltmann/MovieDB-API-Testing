056b3e9123203d675365947a325a6606120a94b1
Michel Altmann <michel.altmann05@gmail.com>
2022-09-06 14:52:06 -0300

    Primeira commit Criação do layout xml principal com o recycler view utilizando o card view

---
+++ app/src/main/java/com/android/desafiokotlin/ui/activity/TopFilmesActivity.kt
@@ -0,0 +1,12 @@
+package com.android.desafiokotlin.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.desafiokotlin.R
+
+class TopFilmesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_filmes)
+    }
+}
