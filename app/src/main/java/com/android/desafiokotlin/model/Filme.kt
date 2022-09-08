21c5739a0a6bae718284bcc9aa3fbda53e50f4b3
Michel Altmann <michel.altmann05@gmail.com>
2022-09-08 18:31:38 -0300

    Comentando codigo do banco de dados Comentado todo o código envolvendo o banco de dados, pois o mesmo não é utilizado no projeto atual

--- app/src/main/java/com/android/desafiokotlin/model/Filme.kt
+++ app/src/main/java/com/android/desafiokotlin/model/Filme.kt
@@ -4,19 +4,29 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-class Filme (
+class Filme(
     @PrimaryKey
     val id: Int?,
     val title: String?,
     val release_date: String?,
-    val backdrop_path: String?
+    val backdrop_path: String?,
+    val overview: String?,
+    val popularity: Number?,
+    val original_title: String?,
+    val vote_average: Number?,
+    val vote_count: Int?
 ){
     val filme: Filme
         get() = Filme(
             id ?: 0,
             title ?: "",
             release_date ?: "",
-            backdrop_path ?: ""
+            backdrop_path ?: "",
+            overview ?: "",
+            popularity ?: 0,
+            original_title ?: "",
+            vote_average ?: 0,
+            vote_count ?: 0
         )
 }
 
